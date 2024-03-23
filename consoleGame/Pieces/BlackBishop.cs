9294cba7696374dad423a68c234a75d75430baea
Oskar <hamerlao@gmail.com>
2024-03-23 23:41:03 +0100

    BlackBishop added

---
+++ consoleGame/Pieces/BlackBishop.cs
@@ -0,0 +1,22 @@
+﻿using consoleGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Pieces
+{
+    public class BlackBishop : WhiteBishop
+    {
+        public BlackBishop(int x, int y) : base(x, y)
+        {
+
+        }
+
+        public new char Symbol { get; set; } = 'b';
+        public override AttackService AttackService { get; } = new AttackService('b');
+
+
+    }
+}
