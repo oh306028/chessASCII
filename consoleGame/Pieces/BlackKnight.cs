b58790b405e9f6dccef69655a936b78e0c4f56c6
Oskar <hamerlao@gmail.com>
2024-03-24 19:26:00 +0100

    Both whiteKnight and blackKnight added

---
+++ consoleGame/Pieces/BlackKnight.cs
@@ -0,0 +1,20 @@
+﻿using consoleGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Pieces
+{
+    public class BlackKnight : WhiteKnight
+    {
+        public BlackKnight(int x, int y) : base(x, y)
+        {
+        }
+
+        public new char Symbol { get; set; } = 'n';
+
+        public override AttackService AttackService { get; } = new AttackService('n');
+    }
+}
---
+++ consoleGame/Pieces/WhiteKnight.cs
@@ -0,0 +1,58 @@
+﻿using consoleGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Pieces
+{
+    public class WhiteKnight : IPiece
+    {
+        public char Symbol { get; set; } = 'N';
+        public int XPosition { get ; set ; }
+        public int YPosition { get ; set; }
+
+        public virtual AttackService AttackService { get; } = new AttackService('N');
+
+        public WhiteKnight(int x, int y)
+        {
+            XPosition = x;
+            YPosition = y;
+        }
+
+
+        public bool CanMove(MoveService move, char[,] board)
+        {
+
+            if(move.XMove == XPosition && move.YMove == YPosition)
+                return false;
+
+
+            if (board[move.XMove, move.YMove] != ' ' && !AttackService.CanAttack(move, board))           
+                return false;
+            
+
+
+            if(board[move.XMove, move.YMove] == ' ' || AttackService.CanAttack(move, board))
+            {
+                if (XPosition - 2 == move.XMove && YPosition - 1 == move.YMove || XPosition - 2 == move.XMove && YPosition + 1 == move.YMove)
+                    return true;
+
+                if (XPosition + 2 == move.XMove && YPosition + 1 == move.YMove || XPosition + 2 == move.XMove && YPosition - 1 == move.YMove)
+                    return true;
+
+                if (XPosition - 1 == move.XMove && YPosition - 2 == move.YMove || XPosition + 1 == move.XMove && YPosition - 2 == move.YMove)
+                    return true;
+
+                if (XPosition + 1 == move.XMove && YPosition + 2 == move.YMove || XPosition - 1 == move.XMove && YPosition + 2 == move.YMove)
+                    return true;
+            }
+
+
+
+            return false;
+
+        }
+    }
+}
