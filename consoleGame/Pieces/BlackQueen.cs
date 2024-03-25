6b4cb78ab61c255226651b89da0e21a8d8c467e3
Oskar <hamerlao@gmail.com>
2024-03-25 19:37:13 +0100

    Black queen added

---
+++ consoleGame/Pieces/BlackQueen.cs
@@ -0,0 +1,21 @@
+﻿using consoleGame.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Pieces
+{
+    public class BlackQueen : WhiteQueen
+    {
+        public BlackQueen(int x, int y) : base(x, y)
+        {
+        }
+
+        public new char Symbol { get; set; } = 'q';
+        public override AttackService  AttackService { get; } = new AttackService('q'); 
+
+
+    }
+}
