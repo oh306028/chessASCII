6d9519679f1a1c708215cacf2892d880dbd7c982
Oskar <hamerlao@gmail.com>
2024-04-06 10:10:56 +0200

    castlingService added

--- consoleGame/Services/CastlingService.cs
+++ consoleGame/Services/CastlingService.cs
@@ -11,25 +11,86 @@ namespace consoleGame.Services
     {
         private readonly ICastlingPiece _king;
         private readonly ICastlingPiece _rook;
+        private readonly char _symbol;
 
-        public CastlingService(ICastlingPiece king, ICastlingPiece rook)
+        private int Index;
+        public CastlingService(ICastlingPiece king, ICastlingPiece rook, char symbol)
         {
             _king = king;
             _rook = rook;
+            _symbol = symbol;
+        }
+
+        public void GetDestinationIndex(int index)
+        {
+            Index = index;  
+        }   
+
+
+        private List<int> GetRoad()
+        {
+            var road = new List<int>();
+
+            for(int i = 0; i < Index; i++)
+            {
+                road.Add(i);
+            }
+
+            return road;
         }
 
 
-        public bool CanCastle()
+
+        private bool IsAttackingTheCastlingRoadByBlack(char[,] board)
+        {
+            var checker = new CheckService(board);
+
+
+            var roadFields = GetRoad();
+
+            foreach(var field in roadFields)
+            {
+                if (checker.BlackCanCheck(8, field))
+                    return false;
+            }
+             
+            return true;
+        }
+
+        private bool IsAttackingTheCastlingRoadByWhite(char[,] board)
+        {   
+            var checker = new CheckService(board);
+
+            var roadFields = GetRoad();
+
+            foreach (var field in roadFields)
+            {
+                if (checker.WhiteCanCheck(1, field))
+                    return false;
+            }
+
+            return true;
+        }
+
+
+
+        public bool CanCastle(char[,] board)
         {
             if (_king.HasMoved || _rook.HasMoved)
                 return false;
-
-
-            // some service to check if at the road between rook and king noone is attacking
-            // should be the checkService to peek at the road fields
-
-            // then can castle
-
+         
+            if(_symbol == 'K')
+            {
+                if (IsAttackingTheCastlingRoadByBlack(board))
+                    return false;
+            }
+          
+            if (_symbol == 'k')
+            {
+                if (IsAttackingTheCastlingRoadByWhite(board))
+                    return false;
+            }
+               
 
             return true;
         }
