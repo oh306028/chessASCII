5e5d2878de71914df4e22e0ac6f1c6d0365a9cb7
Oskar <hamerlao@gmail.com>
2024-04-06 08:55:27 +0200

    CheckService tests added

---
+++ consoleGame.Test/CheckServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using consoleGame.Pieces;
+using consoleGame.Services;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Test
+{
+    public class CheckServiceTests
+    {
+        [Fact]
+        public void CheckService_ForValidCheckBlackRook_ReturnsTrue()
+        {
+            //init
+                
+            var board = Board.InitGrid();
+
+
+            board[7, 5] = ' ';
+            board[6, 5] = 'r';
+
+            var checkService = new CheckService(board);
+
+            //act
+
+            var result = checkService.BlackCanCheck();  
+
+
+            //assert    
+
+            result.Should().Be(true);
+        }
+
+        [Fact]
+        public void CheckService_ForValidCheckBlackKnight_ReturnsTrue()
+        {
+            //init
+
+            var board = Board.InitGrid();
+
+
+            board[7, 5] = ' ';
+            board[6, 6] = 'n';
+
+            var checkService = new CheckService(board);
+
+            //act
+
+            var result = checkService.BlackCanCheck();
+
+
+            //assert
+
+            result.Should().Be(true);
+        }
+
+        [Fact]
+        public void CheckService_ForValidCheckWhiteRook_ReturnsTrue()
+        {   
+            //init
+
+            var board = Board.InitGrid();
+
+
+            board[2, 5] = ' ';
+            board[6, 5] = 'R';
+
+            var checkService = new CheckService(board);
+
+            //act
+
+            var result = checkService.WhiteCanCheck();
+
+
+            //assert
+
+            result.Should().Be(true);
+        }
+    }
+}
