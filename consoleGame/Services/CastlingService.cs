a9ec8a1cf40d30e5707da793ec7ea9e4a4628116
Oskar <hamerlao@gmail.com>
2024-04-06 08:53:56 +0200

    CastlingService added

---
+++ consoleGame/Services/CastlingService.cs
@@ -0,0 +1,37 @@
+﻿using consoleGame.Pieces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleGame.Services
+{
+    public class CastlingService
+    {
+        private readonly ICastlingPiece _king;
+        private readonly ICastlingPiece _rook;
+
+        public CastlingService(ICastlingPiece king, ICastlingPiece rook)
+        {
+            _king = king;
+            _rook = rook;
+        }
+
+
+        public bool CanCastle()
+        {
+            if (_king.HasMoved || _rook.HasMoved)
+                return false;
+
+
+            // some service to check if at the road between rook and king noone is attacking
+            // should be the checkService to peek at the road fields
+
+            // then can castle
+
+
+            return true;
+        }
+    }
+}
