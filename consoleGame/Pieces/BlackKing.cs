7dda467aa1d724455f7ae5e98c802ae63562dcfa
Oskar <hamerlao@gmail.com>
2024-04-15 19:29:56 +0200

    Refactored kings to work with castles

--- consoleGame/Pieces/BlackKing.cs
+++ consoleGame/Pieces/BlackKing.cs
@@ -14,7 +14,56 @@ namespace consoleGame.Pieces
 
         }
         public new char Symbol { get; set; } = 'k';
+
         public override AttackService AttackService { get; } = new AttackService('k');
+
+        public override bool CanMove(MoveService move, char[,] board)
+        {
+
+            if (move.XMove == XPosition && move.YMove == YPosition)
+                return false;
+
+
+            if (board[move.XMove, move.YMove] != ' ' && !AttackService.CanAttack(move, board))
+                return false;
+
+
+            if (XPosition == move.XMove && (YPosition + 1 == move.YMove || YPosition - 1 == move.YMove))
+            {
+                if (ValidMove(move, board)) { HasMoved = true; return true; }
+            }
+
+
+            if (YPosition == move.YMove && (XPosition + 1 == move.XMove || XPosition - 1 == move.XMove))
+            {
+                if (ValidMove(move, board)) { HasMoved = true; return true; }
+            }
+
+
+            if (Math.Abs(XPosition - move.XMove) == 1 && Math.Abs(YPosition - move.YMove) == 1)
+            {
+                if (ValidMove(move, board)) { HasMoved = true; return true; }
+            }
+
+
+
+
+            if (board[move.XMove, move.YMove - 1] == 'r' || board[move.XMove, move.YMove + 1] == 'r')
+            {
+               
+                //castling
+
+                var castler = new CastlingService(new BlackKing(XPosition, YPosition), new BlackRook(move.XMove, move.YMove + 1), 'k');
+
+                castler.GetDestinationIndex(YPosition, move.YMove);
+
+                if (castler.CanCastle(board))
+                { HasMoved = true; return true; }
+
+            }
+
+            return false;
+        }
 
 
     }
--- consoleGame/Pieces/WhiteKing.cs
+++ consoleGame/Pieces/WhiteKing.cs
@@ -22,14 +22,14 @@ namespace consoleGame.Pieces
             YPosition = y;
         }
 
-        private bool ValidMove(MoveService move, char[,] board)
+        protected  bool ValidMove(MoveService move, char[,] board)
         {
             if (board[move.XMove, move.YMove] == ' ' || (board[move.XMove, move.YMove] != ' ' && AttackService.CanAttack(move, board))) return true;
 
             return false;
         }
 
-        public bool CanMove(MoveService move, char[,] board)
+        public virtual bool CanMove(MoveService move, char[,] board)
         {
 
             if (move.XMove == XPosition && move.YMove == YPosition)
@@ -49,14 +49,14 @@ namespace consoleGame.Pieces
                 if (YPosition == move.YMove && (XPosition + 1 == move.XMove || XPosition - 1 == move.XMove))
                 {
                    if (ValidMove(move, board)) { HasMoved = true; return true; }
-            }
+                }
 
 
                 if (Math.Abs(XPosition - move.XMove) == 1 && Math.Abs(YPosition - move.YMove) == 1)
                 {
                     if (ValidMove(move, board)) { HasMoved = true; return true; }
-            }
-            
+                }
+             
                       
 
                 //castling
@@ -71,20 +71,6 @@ namespace consoleGame.Pieces
                    { HasMoved = true; return true; }
 
                 }
-
-
-                if (board[move.XMove, move.YMove - 1] == 'r' || board[move.XMove, move.YMove + 1] == 'r')
-                {
-
-                 var castler = new CastlingService(new BlackKing(XPosition, YPosition), new BlackRook(move.XMove, move.YMove - 1), 'k');    
-
-                castler.GetDestinationIndex(YPosition, move.YMove);
-
-                if (castler.CanCastle(board))
-                  { HasMoved = true; return true; }
-
-                }
-
 
                 return false;
 
